af18dcc83af845f0d5ce890d43b57f4c3d242bd6
mithy45 <quentin.guyot.etu@gmail.com>
2023-04-19 12:42:03 +0200

    WPF ListBox Binding with TextBox

---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DataProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Person> persons = new List<Person>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (var person in new PersonRepository().GetAll())
+            {
+                persons.Add(person);
+            }
+
+            myListBox.ItemsSource = persons;
+        }
+    }
+}
